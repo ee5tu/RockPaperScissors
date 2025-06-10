9765984a2fa466474eae99b21dc1f2fb1ade374d
ee5tu <sir.eetu@gmail.com>
2025-06-10 21:43:27 +0300

    Implemented logic for one round

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log('Hello');
+//get computer and human's choice
+
+const getComputerChoice = function () {
+	const choice = Math.trunc(Math.random() * 3);
+
+	if (choice === 0) {
+		return 'rock';
+	} else if (choice === 1) {
+		return 'paper';
+	} else {
+		return 'scissors';
+	}
+};
+
+const getHumanChoice = function () {
+	const choice = prompt(
+		'Enter your choice of Rock, Paper or Scissors: '
+	).toLowerCase();
+
+	return choice;
+};
+
+//scores
+const computerScore = 0;
+const humanScore = 0;
+
+//win messages
+const computerW = 'Computer wins!';
+const humanW = 'You win!';
+
+//play mechanic
+
+const playRound = function (humanChoice, computerChoice) {
+	if (humanChoice === computerChoice) {
+		console.log('TIE');
+	} else if (humanChoice === 'rock') {
+		if (computerChoice === 'paper') {
+			console.log(computerW);
+		} else console.log(humanW);
+	} else if (humanChoice === 'paper') {
+		if (computerChoice === 'scissors') {
+			console.log(computerW);
+		} else console.log(humanW);
+	} else if (humanChoice === 'scissors') {
+		if (computerChoice === 'rock') {
+			console.log(computerW);
+		} else console.log(humanW);
+	} else console.log('Something went wrong.');
+};
+
+playRound(getHumanChoice(), getComputerChoice());
+
+const playGame = function () {};
